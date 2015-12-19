7f80cee2123eff2166e1a12976b23699d6e4a860
Isabel Diaz Miranda <isabeldiazmi@gmail.com>
2015-12-18 18:37:43 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Isabel Díaz Miranda on 18/12/15.
+//  Copyright © 2015 Isabel Díaz Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
